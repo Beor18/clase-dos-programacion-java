9bd8d34e8a7345de2b33aaa00d8d517ade6a3944
Fernando Lopez <fer.ragnar19@gmail.com>
2018-08-17 09:58:22 -0300

    desafio terminado

--- src/NumerosPrimosEntre0y10/NumerosPrimosEntre0y10.java
+++ src/NumerosPrimosEntre0y10/NumerosPrimosEntre0y10.java
@@ -3,4 +3,31 @@ package NumerosPrimosEntre0y10;
 
 public class NumerosPrimosEntre0y10 {
     
+    int numeroPrimoDos = 2;
+    int numeroPrimoTres = 3;
+    int numeroPrimoCinco = 5;
+    int numeroPrimoSiete = 7;
+    private float sumaDePrimos = numeroPrimoDos + numeroPrimoTres + numeroPrimoCinco + numeroPrimoSiete;;
+    
+  public float numerosPrimos(){
+    
+    return(sumaDePrimos);
+    
+  
+  }
+  
+  public float multiplicarPrimo(){
+  
+    float multiplicacionDePrimos = numeroPrimoDos * numeroPrimoTres * numeroPrimoCinco * numeroPrimoSiete;
+    return (multiplicacionDePrimos);
+  
+  }
+  
+  public float promedioPrimos(){
+  
+      float promedioDePrimos = sumaDePrimos / 4f;
+      return(promedioDePrimos);
+  
+  }
+    
 }
--- src/ProgramaPrincipal/ProgramaPrincipal.java
+++ src/ProgramaPrincipal/ProgramaPrincipal.java
@@ -1,10 +1,21 @@
-
 package ProgramaPrincipal;
+
+import NumerosPrimosEntre0y10.NumerosPrimosEntre0y10;
 
 
 public class ProgramaPrincipal {
     public static void main(String[] args){
-        System.out.println("primera class");
-    
+        NumerosPrimosEntre0y10 numeros = new NumerosPrimosEntre0y10();
+        System.out.println("Operacion Uno");
+        System.out.println(numeros.numerosPrimos());
+        System.out.println("------------------------------------");
+        
+        System.out.println("Operacion Dos");
+        System.out.println(numeros.multiplicarPrimo());
+        System.out.println("------------------------------------");
+        
+        System.out.println("Operacion Tres");
+        System.out.println(numeros.promedioPrimos());
+        System.out.println("------------------------------------");
     }
 }
