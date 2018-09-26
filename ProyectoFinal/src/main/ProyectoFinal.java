430c3bfb2fc7631a40bf4013574932af36469ffd
Beor18 <fer.ragnar19@gmail.com>
2018-09-26 09:35:53 -0300

    proyecto final

---
+++ ProyectoFinal/src/main/ProyectoFinal.java
@@ -0,0 +1,11 @@
+package main;
+import client.PrimeraPantalla;
+
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        PrimeraPantalla pantallaPrincipal = new PrimeraPantalla();
+        pantallaPrincipal.setVisible(true);
+    }
+    
+}
