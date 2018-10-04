5f6a60caea58009081105412d4a902a6b3700bc9
Beor18 <fer.ragnar19@gmail.com>
2018-10-03 22:44:41 -0300

    ejemplos en java

---
+++ EjemploUsoListas/src/main/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package main;
+
+import presentation.VentanaPrincipal;
+
+public class ProgramaPrincipal {
+    
+    public static void main(String[] args){
+        //Inicio la ventana principal
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
---
+++ ProyectoFinalAvanzado/src/main/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package main;
+
+import cliente.VentanaPrincipal;
+
+public class ProgramaPrincipal {
+    
+    public static void main(String[] args){
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
